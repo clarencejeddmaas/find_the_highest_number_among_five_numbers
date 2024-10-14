674f5999149fe387b96045c7e4d999c05c60259b
clarencejeddmaas <clarencemaas74@gmail.com>
2024-10-14 16:10:59 +0800

    Ask the user for five numbers

---
+++ highest_number_among_five_numbers.py
@@ -0,0 +1,8 @@
+def highest_number():
+
+    # Ask the user to input 5 numbers
+    num_1 = float(input("Enter the first number: "))
+    num_2 = float(input("Enter the second number: "))
+    num_3 = float(input("Enter the third number: "))
+    num_4 = float(input("Enter the fourth number: "))
+    num_5 = float(input("Enter the fifth number: "))
